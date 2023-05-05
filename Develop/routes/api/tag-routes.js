3c20e5dd215a603c6bf914d280263ff7b92dfaaf
Sami <coolsweetsami@gmail.com>
2023-05-04 23:18:58 -0400

    fixes to bug in tag

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -5,9 +5,16 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', async(req, res) => {
   try {
-    const tagData = await Tag.findAll(req.params.id,{
+    const tagData = await Tag.findAll({
       include: [
-        { model: Product }
+        { 
+          model: Product, 
+          through: { 
+            model: ProductTag, 
+            attributes: [] 
+          },
+          as: 'product'
+        }
       ]
     });
     res.status(200).json(tagData);
@@ -15,11 +22,14 @@ router.get('/', async(req, res) => {
     res.status(500).json(err);
   }
 });
+
 router.get('/:id', async (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
-  try {
-    const tagData = await Tag.findByPk(req.params.id);
+   try {
+    const tagData = await Tag.findByPk(req.params.id,{ 
+      include: [
+      { model: Product }
+    ]
+  });
 
     if (!tagData) {
       res.status(404).json({ message: 'No tag found with this id!' });
