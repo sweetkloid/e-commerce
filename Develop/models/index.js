4a69c59615e4209ab98670702825d9ecf179b908
Sami <coolsweetsami@gmail.com>
2023-05-04 20:27:57 -0400

    get all route for categories added

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,33 +4,34 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+//product belongs to category
 Product.belongsTo(Category, {
+   //referencing the primary key of category
   foreignKey: 'category_id',
-  as: 'category'
+  onDelete: 'CASCADE'
 })
 
-// Categories have many Products
-Category.hasMany(Product, { 
-  foreignKey: 'category_id',
-  as: 'category'
+//product is allowed to have multiple categories
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE'
 });
 
-// Products belongToMany Tags (through ProductTag)
+// this junction connects 2 models together, product belongs to tag and has multiple tags
 Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
     unique: false
   },
-  as: 'product_tag'
+  as: 'producttag'
 });
-// Tags belongToMany Products (through ProductTag)
+// tag belongs to product and has many 
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
     unique: false
   },
-  as: 'product_tag'
+  as: 'producttag'
 });
 
 module.exports = {
